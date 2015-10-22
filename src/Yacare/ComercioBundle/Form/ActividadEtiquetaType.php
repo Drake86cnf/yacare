0d9454f763dd0076700bd4ce4f5fa7d217d3ee21
Ernesto Nicolás Carrea <equistango@gmail.com>
2015-10-22 11:29:05 -0300

    Edición y listado de etiquetas de actividades.

---
+++ src/Yacare/ComercioBundle/Form/ActividadEtiquetaType.php
@@ -0,0 +1,33 @@
+<?php
+namespace Yacare\ComercioBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Formulario para una etiqueta de actividad.
+ *
+ * @author Ernesto Carrea <ernestocarrea@gmail.com>
+ * @author Alejandro Díaz <alediaz.rc@gmail.com>
+ */
+class ActividadEtiquetaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Codigo', null, array(
+                'label' => 'Código',
+                'attr' => array('class' => 'tapir-input-160'),
+                'required' => true))
+            ->add('Nombre', null, array(
+                'label' => 'Nombre',
+                'required' => true))
+            ;
+    }
+
+    public function getName()
+    {
+        return 'yacare_comerciobundle_actividadetiquetatype';
+    }
+}
