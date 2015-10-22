60b040b0d1d7c1ab6e4842a323dbe8ad5300109a
Ernesto Nicolás Carrea <equistango@gmail.com>
2015-10-22 09:26:47 -0300

    Cosas que no tienen que ver con flota.

---
+++ src/Yacare/InspeccionBundle/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Yacare\InspeccionBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+/**
+ * Controlador de inicio.
+ * 
+ * @author Ernesto Carrea <ernestocarrea@gmail.com>
+ * 
+ * @Route("default/")
+ */
+class DefaultController extends \Tapir\BaseBundle\Controller\DefaultController
+{
+}
