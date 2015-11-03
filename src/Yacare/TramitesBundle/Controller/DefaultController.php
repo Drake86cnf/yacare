1e0b352e40cab9af40f8a471530d4a89545bd5f4
Ezequiel Riquelme <rezequiel.tdf@gmail.com>
2015-11-03 09:10:00 -0300

    Agregando inicio en Trámites + pequeñas correcciones.

---
+++ src/Yacare/TramitesBundle/Controller/DefaultController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Yacare\TramitesBundle\Controller;
+
+/**
+ * Controlador de inicio
+ * 
+ * @author Ezequiel Riquelme <rezequiel.tdf@gmail.com>
+ */
+class DefaultController extends \Tapir\BaseBundle\Controller\DefaultController
+{
+}
