72d508ce9f5e47fa335c57a2fc317930e0cfd012
Ezequiel Riquelme <rezequiel.tdf@gmail.com>
2015-11-03 09:10:00 -0300

    Agregando inicio en Trámites + pequeñas correcciones.

--- src/Yacare/ObrasParticularesBundle/Controller/DefaultController.php
+++ src/Yacare/ObrasParticularesBundle/Controller/DefaultController.php
@@ -8,7 +8,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
  * Controlador de inicio.
  * 
  * @author Ernesto Carrea <ernestocarrea@gmail.com>
- * 
  */
 class DefaultController extends \Tapir\BaseBundle\Controller\DefaultController
 {
