09d340e31b945fa2081f922ab503c1fc3d11561e
Ezequiel Riquelme <rezequiel.tdf@gmail.com>
2015-11-04 10:55:44 -0300

    Actualizando vista de inicio en Trámites + estilo hoverselect para tablas.

--- src/Yacare/TramitesBundle/Controller/DefaultController.php
+++ src/Yacare/TramitesBundle/Controller/DefaultController.php
@@ -1,6 +1,9 @@
 <?php
 namespace Yacare\TramitesBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 /**
  * Controlador de inicio
  * 
@@ -8,4 +11,31 @@ namespace Yacare\TramitesBundle\Controller;
  */
 class DefaultController extends \Tapir\BaseBundle\Controller\DefaultController
 {
+    /**
+     * Controlador de inicio.
+     * 
+     * @see \Tapir\BaseBundle\Controller\DefaultController::inicioAction() DefaultController::inicioAction()
+     * 
+     * @Route("inicio/")
+     * @Template()
+     */
+    public function inicioAction(\Symfony\Component\HttpFoundation\Request $request)
+    {
+        $res = parent::inicioAction($request);
+        
+        $entitiesRecientes = array();
+        $em = $this->getEm();
+        $entities = $em->getRepository('YacareTramitesBundle:Requisito')->findBy(array('Tipo' => 'ext'));
+        
+        foreach ($entities as $entity) {
+            $ultimaActualizacion = $entity->getUpdatedAt()->diff(new \DateTime());
+            if ($ultimaActualizacion->days <= 10) {
+                $entitiesRecientes[] = $entity;
+            }
+        }
+        $res['entities'] = $entitiesRecientes;
+        $res['entitiesCantidad'] = count($entities);
+        
+        return $res;
+    }
 }
