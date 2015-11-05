2d1b1695382dc0a2113db91b8e359aab9e19481a
Ernesto Nicolás Carrea <equistango@gmail.com>
2015-11-05 10:30:17 -0300

    Resultado de editar.

---
+++ src/Tapir/AbmBundle/Helper/Resultados/ResultadoEditarGuardarAction.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tapir\AbmBundle\Helper\Resultados;
+
+/**
+ * Describe el resultado de una acción de editar una.
+ *  
+ * @author Ernesto Carrea <ernestocarrea@gmail.com>
+ */
+class ResultadoEditarGuardarAction extends ResultadoActionAbmController {
+    function __construct($controlador) {
+        parent::__construct($controlador);
+    }
+    
+    public $Entidad;
+    public $FormularioEditar;
+    public $FormularioEliminar;
+    public $Errores;
+    
+    /**
+     * Indica la acción a la cual lleva el formulario de editar.
+     * 
+     * Si no se especifica ninguna, el predeterminado es 'guardar'.
+     * Se puede especificar una ruta relativa, como 'guardar' o absoluta como 'vendor_bundle_accion'.
+     */
+    public $AccionGuardar;
+    
+    public function EsCrear() {
+        if($this->Entidad->getId()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public function TieneEliminar() {
+        return $this->FormularioEliminar != null;
+    }
+    
+    public function ObtenerRutaAccionGuardar() {
+        if($this->AccionGuardar) {
+            if(strpos($this->AccionGuardar, '_') === false) {
+                return $this->ObtenerRutaAccion($this->AccionGuardar);
+            } else {
+                return $this->AccionGuardar;
+            }
+        } else {
+            return $this->ObtenerRutaAccion('guardar');
+        }
+    }
+}
