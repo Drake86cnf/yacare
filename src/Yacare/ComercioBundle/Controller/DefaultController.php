e217b44fcd1337946dd68d15bb2cc8b9cbbc1469
alediazrc <alediaz.rc@gmail.com>
2015-11-05 11:02:32 -0300

    Paneles en la vista de inicio de comercio.

--- src/Yacare/ComercioBundle/Controller/DefaultController.php
+++ src/Yacare/ComercioBundle/Controller/DefaultController.php
@@ -22,7 +22,11 @@ class DefaultController extends \Tapir\BaseBundle\Controller\DefaultController
         
         $Tramites = $em->createQuery(
             'SELECT r FROM Yacare\ComercioBundle\Entity\TramiteHabilitacionComercial r WHERE r.Estado<90')->getResult();
+        $Comercios = $em->createQuery(
+            'SELECT r FROM Yacare\ComercioBundle\Entity\Comercio r WHERE r.Estado=1')->getResult();
+        $Locales = $em->createQuery(
+            'SELECT r FROM Yacare\ComercioBundle\Entity\Local r')->getResult();
         
-        return $this->ArrastrarVariables($request, array('tramites' => $Tramites));
+        return $this->ArrastrarVariables($request, array('tramites' => $Tramites, 'comercios'=>$Comercios, 'locales'=>$Locales));
     }
 }
