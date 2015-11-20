b7bb86e6d71faa0427b04be4c7166d5ab2b5d973
Ernesto Nicolás Carrea <equistango@gmail.com>
2015-11-20 12:40:12 -0300

    Recalcular y mostrar NombreVisible.

---
+++ src/Yacare/BaseBundle/EventListener/BaseListener.php
@@ -0,0 +1,41 @@
+<?php
+namespace Yacare\BaseBundle\EventListener;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\Common\EventSubscriber;
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+
+/**
+ * Escucha los eventos "lifecycle" de Doctrine para intervenir durante la creación o modificación de ciertas entidades.
+ *
+ * @author Ernesto Carrea <ernestocarrea@gmail.com>
+ */
+class BaseListener implements EventSubscriber
+{
+    private $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if (is_a($entity, 'Yacare\BaseBundle\Entity\Persona')) {
+            // Capturo los eventos si la entidad es un trámite
+            $Helper = new \Yacare\BaseBundle\Helper\PersonaHelper();
+            $Helper->LifecycleEvent($args);
+        }
+    }
+
+    public function preUpdate(LifecycleEventArgs $args)
+    {
+        return $this->prePersist($args);
+    }
+
+    public function getSubscribedEvents()
+    {
+        return [\Doctrine\ORM\Events::prePersist, \Doctrine\ORM\Events::preUpdate];
+    }
+}
