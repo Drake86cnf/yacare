d097bd73578a6c953ffa6d75b4cc8a743d7e89b7
Ernesto Nicolás Carrea <equistango@gmail.com>
2015-11-24 16:24:09 -0300

    Buscar actas y comercios por varios campos.

--- src/Yacare/ComercioBundle/Controller/ActaComercioController.php
+++ src/Yacare/ComercioBundle/Controller/ActaComercioController.php
@@ -3,6 +3,7 @@ namespace Yacare\ComercioBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
  * Controlador de actas de comercio.
@@ -17,7 +18,39 @@ class ActaComercioController extends \Tapir\AbmBundle\Controller\AbmController
     {
         parent::IniciarVariables();
         
+        $this->BuscarPor = null;
         $this->OrderBy = 'Fecha DESC';
+    }
+
+    /**
+     * @Route("listar/")
+     * @Template()
+     */
+    public function listarAction(Request $request)
+    {
+        $filtro_buscar = $this->ObtenerVariable($request, 'filtro_buscar');
+    
+        if ($filtro_buscar) {
+            $this->Joins[] = " LEFT JOIN r.Comercio c";
+            $this->Joins[] = " LEFT JOIN c.Titular t";
+            $this->Joins[] = " LEFT JOIN c.Local l";
+    
+            // Busco por varias palabras
+            // cambio , por espacio, quito espacios dobles y divido la cadena en los espacios
+            $palabras = explode(' ', str_replace('  ', ' ', str_replace(',', ' ', $filtro_buscar)), 5);
+            foreach ($palabras as $palabra) {
+                $this->Where .= " AND (c.Nombre LIKE '%$palabra%'
+                OR r.Numero LIKE '%$palabra%'
+                OR l.Nombre LIKE '%$palabra%'
+                OR t.NombreVisible LIKE '%$palabra%'
+                OR t.RazonSocial LIKE '%$palabra%'
+                OR t.DocumentoNumero LIKE '%$palabra%'
+                OR t.Cuilt LIKE '%$palabra%')";
+            }
+        }
+        $res = parent::listarAction($request);
+    
+        return $res;
     }
     
     protected function CrearNuevaEntidad(Request $request)
