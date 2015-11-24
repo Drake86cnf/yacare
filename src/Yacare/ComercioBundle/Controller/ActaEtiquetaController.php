ae8e25ae781c6982d619997182d7f9c6de8df349
Ernesto Nicolás Carrea <equistango@gmail.com>
2015-11-24 12:31:16 -0300

    Mejoras en actas, etiquetas.

---
+++ src/Yacare/ComercioBundle/Controller/ActaEtiquetaController.php
@@ -0,0 +1,17 @@
+<?php
+namespace Yacare\ComercioBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Controlador de etiquetas de actas.
+ * 
+ * @author Ernesto Carrea <ernestocarrea@gmail.com>
+ * 
+ * @Route("actaetiqueta/")
+ * @Template()
+ */
+class ActaEtiquetaController extends \Tapir\AbmBundle\Controller\AbmController
+{
+}
