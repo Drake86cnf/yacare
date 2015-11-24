24a1ae2b9ad78489b88ccc8bc2e532e0586a7970
Ernesto Nicolás Carrea <equistango@gmail.com>
2015-11-24 15:14:54 -0300

    Sanitización y mejoras en ConExpediente y ConActoAdministrativo.

--- src/Yacare/AdministracionBundle/Entity/ConActoAdministrativo.php
+++ src/Yacare/AdministracionBundle/Entity/ConActoAdministrativo.php
@@ -20,26 +20,34 @@ trait ConActoAdministrativo
      * 
      * @ORM\Column(type="string", length=255, nullable=true)
      * @Assert\Regex(
-     *     pattern="/^\s*(DM|RM|DC|RC|DJ|RJ|SI|SG|SF|SA|SO|SP|AD|OR)\-(\d{1,5})\/(19|20)(\d{2})\s*$/i",
+     *     pattern="/^\s*(DM|RM|DC|RC|DJ|RJ|SI|SG|SF|SA|SO|SP|AD|OR)*(\-)*(\d{1,5})\/(19|20)(\d{2})\s*$/i",
      *     message="Debe escribir el número de acto administrativo en el formato DM-1234/2015, RC-321/2014, etc."
      * )
      */
     protected $ActoAdministrativoNumero;
 
     /**
+     * Setter con sanitización.
+     */
+    public function setActoAdministrativoNumero($actoAdministrativoNumero)
+    {
+        if($actoAdministrativoNumero) {
+            // Lo paso a mayúsculas y le agrego el guió si corresponde
+            $actoAdministrativoNumero = trim(strtoupper($actoAdministrativoNumero));
+            if(strpos($actoAdministrativoNumero, '-') === false) {
+                $actoAdministrativoNumero = substr($actoAdministrativoNumero, 0, 2) . '-' . substr($actoAdministrativoNumero, 2);
+            }
+        }
+    
+        $this->ActoAdministrativoNumero = $actoAdministrativoNumero;
+        return $this;
+    }
+    
+    /**
      * @ignore
      */
     public function getActoAdministrativoNumero()
     {
         return $this->ActoAdministrativoNumero;
-    }
-
-    /**
-     * @ignore
-     */
-    public function setActoAdministrativoNumero($ActoAdministrativoNumero)
-    {
-        $this->ActoAdministrativoNumero = $ActoAdministrativoNumero;
-        return $this;
     }
 }
