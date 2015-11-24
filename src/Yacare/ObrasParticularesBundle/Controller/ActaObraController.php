97baf8a0836e0b2f6a1618fa8c565205da8fc6d8
Ezequiel Riquelme <rezequiel.tdf@gmail.com>
2015-11-24 12:53:33 -0300

    Emisión de descargo de actas.

--- src/Yacare/ObrasParticularesBundle/Controller/ActaObraController.php
+++ src/Yacare/ObrasParticularesBundle/Controller/ActaObraController.php
@@ -36,9 +36,75 @@ class ActaObraController extends \Tapir\AbmBundle\Controller\AbmController
      * @Route("emitirdescargo/")
      * @Template()
      */
-    public function emitirdescargoAction() 
+    public function emitirdescargoAction(Request $request) 
     {
-        return array();
+        $id = $this->ObtenerVariable($request, 'id');
+        $em = $this->getEm();
+        
+        if ($id) {
+            $entity = $this->ObtenerEntidadPorId($id);
+            
+            if ($entity->getPlazo() || $entity->getFechaDescargo()) {
+                return $this->redirect($this->generateUrl($this->obtenerRutaBase('verdescargo'),
+                    $this->ArrastrarVariables($request, array('id' => $id), false)));
+            }
+        }
+        
+        if (! $entity) {
+            throw $this->createNotFoundException('No se puede encontrar la entidad.');
+        }
+        
+        $FormEditarBuilder = $this->createFormBuilder($entity);
+        
+        $FormEditarBuilder
+            ->add('Plazo', 'choice', array(
+                'choices' => array(
+                    '1' => '1 día',
+                    '5' => '5 días',
+                    '10' => '10 días',
+                    '30' => '30 días',
+                    '60' => '60 días',
+                    '90' => '90 días'
+                ),
+                'label' => 'Plazo',
+                'required' => true));
+        
+        $FormEditar = $FormEditarBuilder->getForm();
+        $FormEditar->handleRequest($request);
+        
+        if ($FormEditar->isValid()) {
+            $entity->setFechaDescargo(new \DateTime());
+            
+            $em->persist($entity);
+            $em->flush();
+        
+            return $this->redirect($this->generateUrl($this->obtenerRutaBase('verdescargo'),
+                $this->ArrastrarVariables($request, array('id' => $id), false)));
+        } else {
+            $children = $FormEditar->all();
+        
+            foreach ($children as $child) {
+                $child->getErrorsAsString();
+            }
+            $Errores = $FormEditar->getErrors(true, true);
+        }
+        
+        if ($Errores) {
+            foreach ($Errores as $error) {
+                $this->get('session')->getFlashBag()->add('danger', $error->getMessage());
+            }
+        } else {
+            $Errores = null;
+        }
+        
+        $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoEditarGuardarAction($this),
+            $request);
+        $res->Entidad = $entity;
+        $res->AccionGuardar = 'emitirdescargo';
+        $res->FormularioEditar = $FormEditar->createView();
+        $res->Errores = $Errores;
+        
+        return array('res' => $res); 
     }
     
     /**
