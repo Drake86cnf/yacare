6279a82be151673300edf1ea38ab8211391c298c
Ernesto Nicolás Carrea <equistango@gmail.com>
2015-11-24 16:24:09 -0300

    Buscar actas y comercios por varios campos.

--- src/Yacare/ComercioBundle/Controller/ComercioController.php
+++ src/Yacare/ComercioBundle/Controller/ComercioController.php
@@ -16,6 +16,43 @@ use Symfony\Component\HttpFoundation\Request;
 class ComercioController extends \Tapir\AbmBundle\Controller\AbmController
 {
     use \Tapir\AbmBundle\Controller\ConBuscar;
+    
+    function IniciarVariables()
+    {
+        parent::IniciarVariables();
+    
+        $this->BuscarPor = null;
+    }
+
+    /**
+     * @Route("listar/")
+     * @Template()
+     */
+    public function listarAction(Request $request)
+    {
+        $filtro_buscar = $this->ObtenerVariable($request, 'filtro_buscar');
+    
+        if ($filtro_buscar) {
+            $this->Joins[] = " LEFT JOIN r.Titular t";
+            $this->Joins[] = " LEFT JOIN r.Local l";
+    
+            // Busco por varias palabras
+            // cambio , por espacio, quito espacios dobles y divido la cadena en los espacios
+            $palabras = explode(' ', str_replace('  ', ' ', str_replace(',', ' ', $filtro_buscar)), 5);
+            foreach ($palabras as $palabra) {
+                $this->Where .= " AND (r.Nombre LIKE '%$palabra%'
+                OR r.ExpedienteNumero LIKE '%$palabra%'
+                OR l.Nombre LIKE '%$palabra%'
+                OR t.NombreVisible LIKE '%$palabra%'
+                OR t.RazonSocial LIKE '%$palabra%'
+                OR t.DocumentoNumero LIKE '%$palabra%'
+                OR t.Cuilt LIKE '%$palabra%')";
+            }
+        }
+        $res = parent::listarAction($request);
+    
+        return $res;
+    }
 
     /**
      * @Route("altamanual/")
