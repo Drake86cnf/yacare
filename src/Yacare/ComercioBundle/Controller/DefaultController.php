73aefd6c0a579c8559528f12db209dd51281baaf
Ernesto Nicolás Carrea <equistango@gmail.com>
2015-11-26 15:00:12 -0300

    Inicio mini.

--- src/Yacare/ComercioBundle/Controller/DefaultController.php
+++ src/Yacare/ComercioBundle/Controller/DefaultController.php
@@ -12,22 +12,44 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  */
 class DefaultController extends \Tapir\BaseBundle\Controller\DefaultController
 {
-
     /**
      * @Route("inicio/")
      * @Template
      */
     public function inicioAction(Request $request)
     {
-        $em = $this->getEm();
-        
         $res = $this->ConstruirResultado(new \Yacare\ComercioBundle\Helper\Resultados\ResultadoInicioAction($this),
             $request);
         
-        $res->Contadores['Local'] = $em->createQuery('SELECT COUNT(r.id) FROM Yacare\ComercioBundle\Entity\Local r WHERE r.Suprimido=0')->getSingleScalarResult();
-        $res->Contadores['Comercio'] = $em->createQuery('SELECT COUNT(r.id) FROM Yacare\ComercioBundle\Entity\Comercio r WHERE r.Suprimido=0')->getSingleScalarResult();
-        $res->Contadores['ActaComercio'] = $em->createQuery('SELECT COUNT(r.id) FROM Yacare\ComercioBundle\Entity\ActaComercio r')->getSingleScalarResult();
+        $this->ObtenerContadoresYRecientes($res);
         
         return array('res' => $res);
+    }
+    
+    
+    /**
+     * @Route("miniinicio/")
+     * @Template
+     */
+    public function miniinicioAction(Request $request)
+    {
+        $res = $this->ConstruirResultado(new \Yacare\ComercioBundle\Helper\Resultados\ResultadoInicioAction($this),
+            $request);
+        
+        $this->ObtenerContadoresYRecientes($res);
+    
+        return array('res' => $res);
+    }
+    
+    public function ObtenerContadoresYRecientes($resultado) {
+        $em = $this->getEm();
+        
+        $resultado->Contadores['Local'] = $em->createQuery('SELECT COUNT(r.id) FROM Yacare\ComercioBundle\Entity\Local r WHERE r.Suprimido=0')->getSingleScalarResult();
+        $resultado->Contadores['Comercio'] = $em->createQuery('SELECT COUNT(r.id) FROM Yacare\ComercioBundle\Entity\Comercio r WHERE r.Suprimido=0')->getSingleScalarResult();
+        $resultado->Contadores['ActaComercio'] = $em->createQuery('SELECT COUNT(r.id) FROM Yacare\ComercioBundle\Entity\ActaComercio r')->getSingleScalarResult();
+        
+        $resultado->Recientes['Comercio'] = $em->createQuery('SELECT r FROM Yacare\ComercioBundle\Entity\Comercio r WHERE r.Suprimido=0 ORDER BY r.updatedAt DESC')->setMaxResults(10)->getResult();
+        
+        return $resultado;
     }
 }
