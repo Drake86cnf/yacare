43b9217add661b2234c9f43b93acd6c7644f6803
Ernesto Nicolás Carrea <equistango@gmail.com>
2015-11-26 16:32:03 -0300

    Inicios y mini inicios.

---
+++ src/Yacare/RequerimientosBundle/Controller/DefaultController.php
@@ -0,0 +1,62 @@
+<?php
+namespace Yacare\RequerimientosBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Controlador de inicio.
+ * 
+ * @author Ernesto Carrea <ernestocarrea@gmail.com>
+ */
+class DefaultController extends \Tapir\BaseBundle\Controller\DefaultController
+{
+    /**
+     * @Route("inicio/")
+     * @Template
+     */
+    public function inicioAction(Request $request)
+    {
+        $res = $this->ConstruirResultado(new \Yacare\ComercioBundle\Helper\Resultados\ResultadoInicioAction($this),
+            $request);
+        
+        $this->ObtenerContadoresYRecientes($res);
+        
+        return array('res' => $res);
+    }
+    
+    
+    /**
+     * @Route("miniinicio/")
+     * @Template
+     */
+    public function miniinicioAction(Request $request)
+    {
+        $res = $this->ConstruirResultado(new \Yacare\ComercioBundle\Helper\Resultados\ResultadoInicioAction($this),
+            $request);
+        
+        $this->ObtenerContadoresYRecientes($res);
+    
+        return array('res' => $res);
+    }
+    
+    
+    public function ObtenerContadoresYRecientes($resultado) {
+        $em = $this->getEm();
+        $UsuarioConectado = $this->get('security.token_storage')->getToken()->getUser();
+        
+        //$resultado->Contadores['Requerimiento'] = $em->createQuery('SELECT COUNT(r.id) FROM Yacare\RequerimientosBundle\Entity\Requerimiento r WHERE r.Estado<50')->getSingleScalarResult();
+        
+        $resultado->Recientes['RequerimientosUsuario'] = $em->createQuery('SELECT r FROM Yacare\RequerimientosBundle\Entity\Requerimiento r WHERE r.Encargado=:encargado AND r.Estado<50')
+            ->setParameter('encargado', $UsuarioConectado)
+            ->setMaxResults(10)
+            ->getResult();
+        $resultado->Recientes['RequerimientosEncargado'] = $em->createQuery('SELECT r FROM Yacare\RequerimientosBundle\Entity\Requerimiento r WHERE r.Usuario=:usuario AND r.Estado<50')
+            ->setParameter('usuario', $UsuarioConectado)
+            ->setMaxResults(10)
+            ->getResult();
+        
+        return $resultado;
+    }
+}
