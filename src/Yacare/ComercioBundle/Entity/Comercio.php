c2526c4f769801d7d6e71a21967049466e798e70
Ernesto Nicolás Carrea <equistango@gmail.com>
2015-12-18 12:12:25 -0300

    Restricción única por número de expediente

--- src/Yacare/ComercioBundle/Entity/Comercio.php
+++ src/Yacare/ComercioBundle/Entity/Comercio.php
@@ -3,6 +3,7 @@ namespace Yacare\ComercioBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * El comercio.
@@ -10,7 +11,11 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @author Ernesto Carrea <ernestocarrea@gmail.com>
  *
  * @ORM\Entity(repositoryClass="Tapir\BaseBundle\Entity\TapirBaseRepository")
- * @ORM\Table(name="Comercio_Comercio")
+ * @ORM\Table(name="Comercio_Comercio", uniqueConstraints={
+ *      @ORM\UniqueConstraint(name="Comercio_ExpedienteNumero_Suprimido", columns={"ExpedienteNumero", "Suprimido"})
+ * })
+ * @UniqueEntity(fields={"ExpedienteNumero", "Suprimido"}, ignoreNull=true, errorPath="ExpedienteNumero",
+ *      message="Ya existe un comercio con el número de expediente proporcionado. No es posible cargar dos comercios con el mismo número de expediente")
  */
 class Comercio
 {
