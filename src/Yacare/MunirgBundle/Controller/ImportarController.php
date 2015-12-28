6cfb5ddad695fb3f62a39ec33ff8a4b427d38d4e
Ernesto Nicolás Carrea <equistango@gmail.com>
2015-12-28 12:00:07 -0300

    Importar agentes.

--- src/Yacare/MunirgBundle/Controller/ImportarController.php
+++ src/Yacare/MunirgBundle/Controller/ImportarController.php
@@ -129,263 +129,6 @@ class ImportarController extends \Tapir\BaseBundle\Controller\BaseController
         }
     }
 
-    /**
-     * @Route("agentes/")
-     * @Template("YacareMunirgBundle:Importar:importar.html.twig")
-     */
-    public function importarAgentesAction(Request $request)
-    {
-        $desde = (int) ($request->query->get('desde'));
-        $cant = 100;
-        
-        mb_internal_encoding('UTF-8');
-        ini_set('display_errors', 1);
-        set_time_limit(600);
-        ini_set('memory_limit', '2048M');
-        
-        $em = $this->getDoctrine()->getManager();
-        
-        $DbRecursos = $this->ConectarRrhh();
-        
-        $importar_importados = 0;
-        $importar_actualizados = 0;
-        $importar_procesados = 0;
-        $log = array();
-        
-        $GrupoAgentes = $em->getRepository('YacareBaseBundle:PersonaGrupo')->find(1);
-        
-        foreach ($DbRecursos->query("SELECT * FROM agentes WHERE legajo = 54") as $Agente) {
-            $entity = $em->getRepository('YacareRecursosHumanosBundle:Agente')->findOneBy(
-                array('ImportSrc' => 'rr_hh.agentes', 'ImportId' => $Agente['legajo']));
-            
-            if (! $entity) {
-                $entity = new \Yacare\RecursosHumanosBundle\Entity\Agente();
-                
-                // Asigno manualmente el ID
-                $entity->setId((int) ($Agente['legajo']));
-                $metadata = $em->getClassMetaData(get_class($entity));
-                $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE);
-                
-                $Persona = $em->getRepository('YacareBaseBundle:Persona')->findOneBy(
-                    array('DocumentoNumero' => trim($Agente['nrodoc'])));
-                
-                if (! $Persona) {
-                    $Persona = new \Yacare\BaseBundle\Entity\Persona();
-                    $Persona->setDocumentoNumero($Agente['nrodoc']);
-                    $Persona->setDocumentoTipo((int) $Agente['tipodoc']);
-                }
-                $Persona->setNombre(StringHelper::Desoraclizar($Agente['name']));
-                $Persona->setApellido(StringHelper::Desoraclizar($Agente['lastname']));
-                if ($Agente['fechanacim']) {
-                    $Persona->setFechaNacimiento(new \DateTime($Agente['fechanacim']));
-                }
-                $Persona->setTelefonoNumero(
-                    trim(
-                        str_ireplace('NO DECLARA', '', $Agente['telefono']) . ' ' .
-                             str_ireplace('NO DECLARA', '', $Agente['celular'])));
-                $Persona->setGenero($Agente['sexo'] == 1 ? 1 : 0);
-                $Persona->setEmail(str_ireplace('NO DECLARA', '', strtolower($Agente['email'])));
-                $Persona->setCuilt(trim($Agente['cuil']));
-                
-                $em->persist($Persona);
-                $em->flush();
-                
-                $entity->setPersona($Persona);
-                
-                $entity->setImportSrc('rr_hh.agentes');
-                $entity->setImportId($Agente['legajo']);
-                
-                $importar_importados ++;
-            } else {
-                $Persona = $entity->getPersona();
-                $importar_actualizados ++;
-            }
-            
-            $Departamento = $em->getRepository('YacareOrganizacionBundle:Departamento')->findOneBy(
-                array(
-                    'ImportSrc' => 'rr_hh.sectores', 
-                    'ImportId' => $Agente['secretaria'] . '.' . $Agente['direccion'] . '.' . $Agente['sector']));
-            
-            if (! $Departamento) {
-                $Departamento = $em->getRepository('YacareOrganizacionBundle:Departamento')->findOneBy(
-                    array(
-                        'ImportSrc' => 'rr_hh.direcciones', 
-                        'ImportId' => $Agente['secretaria'] . '.' . $Agente['direccion']));
-            }
-            
-            if (! $Departamento) {
-                $Departamento = $em->getRepository('YacareOrganizacionBundle:Departamento')->findOneBy(
-                    array('ImportSrc' => 'rr_hh.secretarias', 'ImportId' => $Agente['secretaria']));
-            }
-            
-            $entity->setDepartamento($Departamento);
-            $entity->setCategoria($Agente['categoria']);
-            $entity->setSituacion($Agente['situacion']);
-            $entity->setFuncion(StringHelper::Desoraclizar($Agente['funcion']));
-            $entity->setBajaMotivo($Agente['motivo']);
-            
-            if ($Agente['excombatie'] == 'S') {
-                $entity->setExCombatiente(1);
-            }
-            
-            if ($Agente['discapacit'] == 'S') {
-                $entity->setDiscapacitado(1);
-            }
-            
-            if ($Agente['manohabil'] == 'I') {
-                $entity->setManoHabil(1);
-            }
-            
-            $entity->setEstudiosNivel($Agente['estudios']);
-            if ($Agente['titulo'] == 999) {
-                $entity->setEstudiosTitulo(null);
-            } else {
-                $entity->setEstudiosTitulo($Agente['titulo']);
-            }
-            
-            if (\Tapir\BaseBundle\Helper\Cbu::EsCbuValida($Agente['cbu'])) {
-                $entity->setCBUCuentaAgente(\Tapir\BaseBundle\Helper\Cbu::FormatearCbu($Agente['cbu']));
-            }
-            
-            // Si no está en el grupo agentes, lo agrego
-            if ($Persona->getGrupos()->contains($GrupoAgentes) == false) {
-                $Persona->getGrupos()->add($GrupoAgentes);
-                $em->persist($Persona);
-            }
-            
-            // Le pongo el número de legajo en la persona
-            if ($entity->getId()) {
-                $Persona->setAgenteId($entity->getId());
-            }
-            
-            if ($Agente['fechaingre']) {
-                $entity->setFechaIngreso(new \DateTime($Agente['fechaingre']));
-            } else {
-                $entity->setFechaIngreso(null);
-            }
-            
-            if (is_null($Agente['fechabaja']) || $Agente['fechabaja'] === '0000-00-00') {
-                $entity->setBajaFecha(null);
-                $entity->setArchivado(false);
-            } else {
-                $entity->setBajaFecha(new \DateTime($Agente['fechabaja']));
-                $entity->setArchivado(true);
-            }
-            
-            if (is_null($Agente['fechanacion'] || $Agente['fechanacion'] === '0000-00-00')) {
-                $entity->setFechaNacionalizacion(null);
-            } else {
-                $entity->setFechaNacionalizacion(new \DateTime($Agente['fechanacion']));
-            }
-            
-            if (is_null($Agente['ult_act_d'] || $Agente['ult_act_d'] === '0000-00-00')) {
-                $entity->setUltimaActualizacionDomicilio(null);
-            } else {
-                $entity->setUltimaActualizacionDomicilio(new \DateTime($Agente['ult_act_d']));
-            }
-            
-            if (is_null($Agente['fecha_psico'] || $Agente['fecha_psico'] === '0000-00-00')) {
-                $entity->setFechaPsicofisico(null);
-            } else {
-                $entity->setFechaPsicofisico(new \DateTime($Agente['ult_act_d']));
-            }
-            
-            if (is_null($Agente['fecha_CBC'] || $Agente['fecha_CBC'] === '0000-00-00')) {
-                $entity->setFechaCertificadoBuenaConducta(null);
-            } else {
-                $entity->setFechaCertificadoBuenaConducta(new \DateTime($Agente['fecha_CBC']));
-            }
-            
-            if (is_null($Agente['fecha_CAP'] || $Agente['fecha_CAP'] === '0000-00-00')) {
-                $entity->setFechaCertificadoAntecedentesPenales(null);
-            } else {
-                $entity->setFechaCertificadoAntecedentesPenales(new \DateTime($Agente['fecha_CAP']));
-            }
-            
-            if (is_null($Agente['fecha_CD'] || $Agente['fecha_CD'] === '0000-00-00')) {
-                $entity->setFechaCertificadoDomicilio(null);
-            } else {
-                $entity->setFechaCertificadoDomicilio(new \DateTime($Agente['fecha_CD']));
-            }
-            
-            if (\Tapir\BaseBundle\Helper\Cbu::EsCbuValida($Agente['cbu'])) {
-                $entity->setCBUCuentaAgente($Agente['cbu']);
-            }
-            
-            if (is_null($Agente['finalcontr'] || $Agente['finalcontr'] === '0000-00-00')) {
-                $entity->setBajaFechaContrato(null);
-            } else {
-                $entity->setBajaFechaContrato(new \DateTime($Agente['finalcontr']));
-            }
-            
-            if ($Agente['decreto2']) {
-                $entity->setBajaDecreto(\Yacare\MunirgBundle\Helper\StringHelper::FormatearDecreto($Agente['decreto2']));
-            }
-            
-            $entity->setSuprimido(false);
-            
-            $em->persist($entity);
-            $em->flush();
-            
-            $importar_procesados ++;
-            $log[] = $Agente['legajo'] . ': ' . (string) $entity . ($entity->getSuprimido() ? '*' : '') . ' -- ' .
-                 (string) $entity->getDepartamento();
-        }
-        
-        $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoEditarGuardarAction($this), 
-            $request);
-        $res->Entidad = $entity;
-        $res->Actualizados = $importar_actualizados;
-        $res->Importados = $importar_importados;
-        $res->Procesados = $importar_procesados;
-        return array('res' => $res);
-    }
-
-    /**
-     * @Route("categoriamovimiento/")
-     * @Template("YacareMunirgBundle:Importar:importar.html.twig")
-     */
-    public function importarHistorialCategoriasAction(Request $request)
-    {
-        $desde = (int) ($request->query->get('desde'));
-        $cant = 100;
-        
-        mb_internal_encoding('UTF-8');
-        ini_set('display_errors', 1);
-        set_time_limit(600);
-        ini_set('memory_limit', '2048M');
-        
-        $em = $this->getDoctrine()->getManager();
-        
-        $DbRecursos = $this->ConectarRrhh();
-        $importar_importados = 0;
-        $importar_actualizados = 0;
-        $importar_procesados = 0;
-        $log = array();
-        foreach ($DbRecursos->query("SELECT * FROM movcategorias WHERE legajo= 3236") as $MovimAgente) {
-            $entity = $em->getRepository('YacareRecursosHumanosBundle:AgenteCategoriaMovim')->findOneBy(array());
-            // 'ImportSrc' => 'rr_hh.movcategorias',
-            // 'ImportId' => $MovimAgente['legajo']
-            
-            if (! $entity) {
-                $entity = new \Yacare\RecursosHumanosBundle\Entity\AgenteCategoriaMovim();
-                $this->Categorias($entity, $MovimAgente);
-                $importar_importados ++;
-            } else {
-                $this->Categorias($entity, $MovimAgente);
-                $importar_actualizados ++;
-            }
-        }
-        log($MovimAgente['legajo']);
-        $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoEditarGuardarAction($this), 
-            $request);
-        $res->Entidad = $entity;
-        $res->Actualizados = $importar_actualizados;
-        $res->Importados = $importar_importados;
-        $res->Procesados = $importar_procesados;
-        return array('res' => $res);    }
-
-
 
     /**
      * @Route("matriculados/")
@@ -544,7 +287,7 @@ class ImportarController extends \Tapir\BaseBundle\Controller\BaseController
         
         fclose($ArchivoMatriculados);
         
-       $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoEditarGuardarAction($this), 
+        $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoEditarGuardarAction($this), 
             $request);
         $res->Entidad = $entity;
         $res->Actualizados = $importar_actualizados;
@@ -679,14 +422,13 @@ class ImportarController extends \Tapir\BaseBundle\Controller\BaseController
         
         fclose($ArchivoCsv);
         
-        $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoEditarGuardarAction($this),
+        $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoEditarGuardarAction($this), 
             $request);
         $res->Entidad = $entity;
         $res->Actualizados = $importar_actualizados;
         $res->Importados = $importar_importados;
         $res->Procesados = $importar_procesados;
         return array('res' => $res);
-        
     }
 
     /**
@@ -706,15 +448,14 @@ class ImportarController extends \Tapir\BaseBundle\Controller\BaseController
         $em = $this->getDoctrine()->getManager();
         
         $ArchivoBomberos = fopen('/home/adiaz/Documentos/Bomberos.csv', 'r');
-        for ($i=0;$i<1770;$i++){
-            $Actividad= $em->getRepository('YacareComercioBundle:Actividad')->findOneBy(array('NivelRiesgo' => 1));
-            if ($Actividad){
+        for ($i = 0; $i < 1770; $i ++) {
+            $Actividad = $em->getRepository('YacareComercioBundle:Actividad')->findOneBy(array('NivelRiesgo' => 1));
+            if ($Actividad) {
                 $Actividad->setNivelRiesgo(2);
                 $em->persist($Actividad);
                 $em->flush();
             }
         }
-         
         
         $importar_importados = 0;
         $importar_actualizados = 0;
@@ -746,6 +487,7 @@ class ImportarController extends \Tapir\BaseBundle\Controller\BaseController
         $res->Procesados = $importar_procesados;
         return array('res' => $res);
     }
+
     /**
      * @Route("ecologia/")
      * @Template("YacareMunirgBundle:Importar:importar.html.twig")
@@ -754,29 +496,30 @@ class ImportarController extends \Tapir\BaseBundle\Controller\BaseController
     {
         $desde = (int) ($request->query->get('desde'));
         $cant = 100;
-    
+        
         mb_internal_encoding('UTF-8');
         ini_set('display_errors', 1);
         set_time_limit(600);
         ini_set('memory_limit', '2048M');
-    
+        
         $em = $this->getDoctrine()->getManager();
-    
+        
         $ArchivoEcologia = fopen('/home/adiaz/Documentos/Ecologia.csv', 'r');
         
         $importar_importados = 0;
         $importar_actualizados = 0;
         $importar_procesados = 0;
-        $Etiqueta= $em->getRepository('YacareComercioBundle:ActividadEtiqueta')->findOneBy(array('Codigo'=> 'req_cat_ecologica'));
+        $Etiqueta = $em->getRepository('YacareComercioBundle:ActividadEtiqueta')->findOneBy(
+            array('Codigo' => 'req_cat_ecologica'));
         while (! feof($ArchivoEcologia)) {
             $Row = fgetcsv($ArchivoEcologia);
-    
+            
             if ($Row && count($Row) > 0 && $Row[0] != null) {
                 $entity = $em->getRepository('YacareComercioBundle:Actividad')->findOneBy(array('ClaeAfip' => $Row[0]));
                 if ($entity) {
-                  if  (!$entity->getEtiquetas($Etiqueta)){
-                      $entity->getEtiquetas()->add($Etiqueta);
-                  }
+                    if (! $entity->getEtiquetas($Etiqueta)) {
+                        $entity->getEtiquetas()->add($Etiqueta);
+                    }
                     $importar_actualizados ++;
                     $importar_importados ++;
                     $em->persist($entity);
@@ -785,10 +528,10 @@ class ImportarController extends \Tapir\BaseBundle\Controller\BaseController
                 $importar_procesados ++;
             }
         }
-    
+        
         feof($ArchivoEcologia);
-    
-        $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoEditarGuardarAction($this),
+        
+        $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoEditarGuardarAction($this), 
             $request);
         $res->Entidad = $entity;
         $res->Actualizados = $importar_actualizados;
--- src/Yacare/MunirgBundle/Helper/Importador/ImportadorAgentes.php
+++ src/Yacare/MunirgBundle/Helper/Importador/ImportadorAgentes.php
@@ -10,73 +10,261 @@ use Tapir\BaseBundle\Helper\StringHelper;
  * 
  * @author Ernesto Nicolás Carrea <equistango@gmail.com>
  */
-class ImportadorAgentes extends Importador {
+class ImportadorAgentes extends Importador
+{
     use \Yacare\MunirgBundle\Helper\Importador\ConConexionAGestion;
-    
-    function __construct($container, $em) {
+    protected $GrupoAgentes;
+
+    function __construct($container, $em)
+    {
         parent::__construct($container, $em);
     }
 
-    public function Inicializar() {
+    public function Inicializar()
+    {
         parent::Inicializar();
         
         $this->ObtenerConexionAGestion();
+        $this->GrupoAgentes = $this->em->getRepository('YacareBaseBundle:PersonaGrupo')->find(1);
     }
 
-    
-    public function ObtenerRegistros($desde, $cantidad) {
+    public function ObtenerRegistros($desde, $cantidad)
+    {
         return $this->DbGestion->query("SELECT * FROM agentes");
     }
-    
-    
-    public function ObtenerCantidadTotal() {
-        $sql = 'SELECT COUNT(codigo) AS CANT FROM agentes';
+
+    public function ObtenerCantidadTotal()
+    {
+        $sql = 'SELECT COUNT(legajo) AS CANT FROM agentes';
         $Registro = $this->DbGestion->query($sql)->fetch();
-        if($Registro) {
-            return (int)($Registro['CANT']); 
+        if ($Registro) {
+            return (int) ($Registro['CANT']);
         } else {
             return 0;
         }
     }
-    
-    
-    public function ImportarRegistro($Row) {
+
+    public function ImportarRegistro($Row)
+    {
         $resultado = new ResultadoLote();
         $resultado->Registros[] = $Row;
         
-        $nombreBueno = StringHelper::Desoraclizar($Row['detalle']);
-        $entity = $this->em->getRepository('YacareOrganizacionBundle:Departamento')->findOneBy(
-            array('ImportSrc' => 'rr_hh.secretarias', 'ImportId' => $Row['codigo']));
+        $entity = $this->em->getRepository('YacareRecursosHumanosBundle:Agente')->findOneBy(
+            array('ImportSrc' => 'rr_hh.agentes', 'ImportId' => $Row['legajo']));
         
         if (! $entity) {
-            $nuevoId = $this->em->createQuery('SELECT MAX(r.id) FROM YacareOrganizacionBundle:Departamento r')
-                ->getSingleScalarResult();
-            $entity = new \Yacare\OrganizacionBundle\Entity\Departamento();
-            $entity->setId(++ $nuevoId);
-            $entity->setRango(30);
-            $entity->setImportSrc('rr_hh.secretarias');
-            $entity->setImportId($Row['codigo']);
-        
+            $entity = new \Yacare\RecursosHumanosBundle\Entity\Agente();
+            
+            // Asigno manualmente el ID
+            $entity->setId((int) ($Row['legajo']));
+            $metadata = $this->em->getClassMetaData(get_class($entity));
+            $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE);
+            
+            $Persona = $this->em->getRepository('YacareBaseBundle:Persona')->findOneBy(
+                array('DocumentoNumero' => trim($Row['nrodoc'])));
+            
+            if (! $Persona) {
+                $Persona = new \Yacare\BaseBundle\Entity\Persona();
+                $Persona->setDocumentoNumero($Row['nrodoc']);
+                $Persona->setDocumentoTipo((int) $Row['tipodoc']);
+            }
+            $Persona->setNombre(StringHelper::Desoraclizar($Row['name']));
+            $Persona->setApellido(StringHelper::Desoraclizar($Row['lastname']));
+            if ($Row['fechanacim']) {
+                $Persona->setFechaNacimiento(new \DateTime($Row['fechanacim']));
+            }
+            $Persona->setTelefonoNumero(trim(str_ireplace('NO DECLARA', '', $Row['telefono']) . ' ' .
+                         str_ireplace('NO DECLARA', '', $Row['celular'])));
+            $Persona->setGenero($Row['sexo'] == 1 ? 1 : 0);
+            $Persona->setEmail(str_ireplace('NO DECLARA', '', strtolower($Row['email'])));
+            $Persona->setCuilt(trim($Row['cuil']));
+            
+            $this->em->persist($Persona);
+            $this->em->flush();
+            
+            $entity->setPersona($Persona);
+            
+            $entity->setImportSrc('rr_hh.agentes');
+            $entity->setImportId($Row['legajo']);
+            
             $resultado->RegistrosNuevos++;
         } else {
             $resultado->RegistrosActualizados++;
+            $Persona = $entity->getPersona();
         }
         
-        if($entity->getNombreOriginal() != $Row['detalle']) {
-            $entity->setNombre($nombreBueno);
-            $entity->setNombreOriginal($Row['detalle']);
+        $Departamento = $this->em->getRepository('YacareOrganizacionBundle:Departamento')->findOneBy(
+            array('ImportSrc' => 'rr_hh.sectores', 
+                'ImportId' => $Row['secretaria'] . '.' . $Row['direccion'] . '.' . $Row['sector']));
+        
+        if (! $Departamento) {
+            $Departamento = $this->em->getRepository('YacareOrganizacionBundle:Departamento')->findOneBy(
+                array('ImportSrc' => 'rr_hh.direcciones', 'ImportId' => $Row['secretaria'] . '.' . $Row['direccion']));
         }
         
-        $entity->setParentNode($this->Ejecutivo);
-        if ($Row['fecha_baja']) {
-                $entity->setSuprimido(true);
-            } else {
-                $entity->setSuprimido(false);
-            }
+        if (! $Departamento) {
+            $Departamento = $this->em->getRepository('YacareOrganizacionBundle:Departamento')->findOneBy(
+                array('ImportSrc' => 'rr_hh.secretarias', 'ImportId' => $Row['secretaria']));
+        }
+        
+        $entity->setDepartamento($Departamento);
+        $entity->setCategoria($Row['categoria']);
+        $entity->setSituacion($Row['situacion']);
+        $entity->setFuncion(StringHelper::Desoraclizar($Row['funcion']));
+        $entity->setBajaMotivo($Row['motivo']);
+        
+        if ($Row['excombatie'] == 'S') {
+            $entity->setExCombatiente(1);
+        }
+        
+        if ($Row['discapacit'] == 'S') {
+            $entity->setDiscapacitado(1);
+        }
+        
+        if ($Row['manohabil'] == 'I') {
+            $entity->setManoHabil(1);
+        }
+        
+        $entity->setEstudiosNivel($Row['estudios']);
+        if ($Row['titulo'] == 999) {
+            $entity->setEstudiosTitulo(null);
+        } else {
+            $entity->setEstudiosTitulo($Row['titulo']);
+        }
+        
+        if (\Tapir\BaseBundle\Helper\Cbu::EsCbuValida($Row['cbu'])) {
+            $entity->setCBUCuentaAgente(\Tapir\BaseBundle\Helper\Cbu::FormatearCbu($Row['cbu']));
+        }
+        
+        // Si no está en el grupo agentes, lo agrego
+        if ($Persona->getGrupos()->contains($this->GrupoAgentes) == false) {
+            $Persona->getGrupos()->add($this->GrupoAgentes);
+            $this->em->persist($Persona);
+        }
+        
+        // Le pongo el número de legajo en la persona
+        if ($entity->getId()) {
+            $Persona->setAgenteId($entity->getId());
+        }
+        
+        if ($Row['fechaingre']) {
+            $entity->setFechaIngreso(new \DateTime($Row['fechaingre']));
+        } else {
+            $entity->setFechaIngreso(null);
+        }
+        
+        if (is_null($Row['fechabaja']) || $Row['fechabaja'] === '0000-00-00') {
+            $entity->setBajaFecha(null);
+            $entity->setArchivado(false);
+        } else {
+            $entity->setBajaFecha(new \DateTime($Row['fechabaja']));
+            $entity->setArchivado(true);
+        }
+        
+        if (is_null($Row['fechanacion'] || $Row['fechanacion'] === '0000-00-00')) {
+            $entity->setFechaNacionalizacion(null);
+        } else {
+            $entity->setFechaNacionalizacion(new \DateTime($Row['fechanacion']));
+        }
+        
+        if (is_null($Row['ult_act_d'] || $Row['ult_act_d'] === '0000-00-00')) {
+            $entity->setUltimaActualizacionDomicilio(null);
+        } else {
+            $entity->setUltimaActualizacionDomicilio(new \DateTime($Row['ult_act_d']));
+        }
+        
+        if (is_null($Row['fecha_psico'] || $Row['fecha_psico'] === '0000-00-00')) {
+            $entity->setFechaPsicofisico(null);
+        } else {
+            $entity->setFechaPsicofisico(new \DateTime($Row['ult_act_d']));
+        }
+        
+        if (is_null($Row['fecha_CBC'] || $Row['fecha_CBC'] === '0000-00-00')) {
+            $entity->setFechaCertificadoBuenaConducta(null);
+        } else {
+            $entity->setFechaCertificadoBuenaConducta(new \DateTime($Row['fecha_CBC']));
+        }
+        
+        if (is_null($Row['fecha_CAP'] || $Row['fecha_CAP'] === '0000-00-00')) {
+            $entity->setFechaCertificadoAntecedentesPenales(null);
+        } else {
+            $entity->setFechaCertificadoAntecedentesPenales(new \DateTime($Row['fecha_CAP']));
+        }
+        
+        if (is_null($Row['fecha_CD'] || $Row['fecha_CD'] === '0000-00-00')) {
+            $entity->setFechaCertificadoDomicilio(null);
+        } else {
+            $entity->setFechaCertificadoDomicilio(new \DateTime($Row['fecha_CD']));
+        }
+        
+        if (\Tapir\BaseBundle\Helper\Cbu::EsCbuValida($Row['cbu'])) {
+            $entity->setCBUCuentaAgente($Row['cbu']);
+        }
+        
+        if (is_null($Row['finalcontr'] || $Row['finalcontr'] === '0000-00-00')) {
+            $entity->setBajaFechaContrato(null);
+        } else {
+            $entity->setBajaFechaContrato(new \DateTime($Row['finalcontr']));
+        }
+        
+        if ($Row['decreto2']) {
+            $entity->setBajaDecreto(\Yacare\MunirgBundle\Helper\StringHelper::FormatearActoAdministrativo($Row['decreto2']));
+        } else {
+            $entity->setBajaDecreto(null);
+        }
+        
+        $entity->setSuprimido(false);
         
         $this->em->persist($entity);
         $this->em->flush();
         
         return $resultado;
     }
+    
+
+    /**
+     * @Route("categoriamovimiento/")
+     * @Template("YacareMunirgBundle:Importar:importar.html.twig")
+     */
+    public function importarHistorialCategoriasAction(Request $request)
+    {
+        $desde = (int) ($request->query->get('desde'));
+        $cant = 100;
+    
+        mb_internal_encoding('UTF-8');
+        ini_set('display_errors', 1);
+        set_time_limit(600);
+        ini_set('memory_limit', '2048M');
+    
+        $em = $this->getDoctrine()->getManager();
+    
+        $DbRecursos = $this->ConectarRrhh();
+        $importar_importados = 0;
+        $importar_actualizados = 0;
+        $importar_procesados = 0;
+        $log = array();
+        foreach ($DbRecursos->query("SELECT * FROM movcategorias WHERE legajo= 3236") as $MovimAgente) {
+            $entity = $em->getRepository('YacareRecursosHumanosBundle:AgenteCategoriaMovim')->findOneBy(array());
+            // 'ImportSrc' => 'rr_hh.movcategorias',
+            // 'ImportId' => $MovimAgente['legajo']
+    
+            if (! $entity) {
+                $entity = new \Yacare\RecursosHumanosBundle\Entity\AgenteCategoriaMovim();
+                $this->Categorias($entity, $MovimAgente);
+                $importar_importados ++;
+            } else {
+                $this->Categorias($entity, $MovimAgente);
+                $importar_actualizados ++;
+            }
+        }
+        log($MovimAgente['legajo']);
+        $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoEditarGuardarAction($this),
+            $request);
+        $res->Entidad = $entity;
+        $res->Actualizados = $importar_actualizados;
+        $res->Importados = $importar_importados;
+        $res->Procesados = $importar_procesados;
+        return array('res' => $res);
+    }
+    
 }
