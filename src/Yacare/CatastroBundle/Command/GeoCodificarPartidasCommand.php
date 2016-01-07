7d32754a933eb5ad144dff4abab5f1e4d74cf667
Ernesto Nicolás Carrea <equistango@gmail.com>
2016-01-07 11:48:21 -0300

    Comando para geocodificar paritdas por lotes

---
+++ src/Yacare/CatastroBundle/Command/GeoCodificarPartidasCommand.php
@@ -0,0 +1,74 @@
+<?php
+namespace Yacare\CatastroBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Yacare\MunirgBundle\Helper\Importador\ImportadorPartidas;
+use Yacare\MunirgBundle\Helper\Importador\ResultadoImportacion;
+
+class GeoCodificarPartidasCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+        ->setName('catastro:partidas:geocod')
+        ->setDescription('GeoCodificar partidas obteniendo datos de ubicación desde OpenStreerMap o Google Maps')
+        ->addOption(
+            'desde',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Número de registro inicial',
+            0)
+        ->addOption(
+            'hasta',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Número de registro final',
+            0)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->getOption('desde')) {
+            $desde = (int)($input->getOption('desde'));
+        } else {
+            $desde = 0;
+        }
+        if ($input->getOption('hasta')) {
+            $hasta = (int)($input->getOption('hasta'));
+        } else {
+            $hasta = 0;
+        }
+        
+        $output->writeln('Geocodificando partidas...');
+
+        $cantidadTotal = $hasta - $desde;
+        $progress = null;
+        
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $Helper = new \Yacare\CatastroBundle\Helper\PartidaHelper($this->getContainer(), $em);
+        $Partidas = $em->getRepository('Yacare\CatastroBundle\Entity\Partida')->findBy(array(),
+            array('id' => 'ASC'),
+            $desde ?: null,
+            $cantidadTotal ?: null);
+        $progress = new ProgressBar($output, count($Partidas));
+        $progress->start();
+        foreach($Partidas as $Partida) {
+            $Helper->ObtenerGeoCoding($Partida);
+            $progress->advance();
+            $em->flush();
+            // Dormir entre unos segundos entre consulta y consulta
+            sleep(rand(10, 20));
+        }
+        $em->clear();
+        
+        $progress->finish();
+        $output->writeln('');
+
+        $output->writeln('Geocodificación terminada, se procesaron ' . count($Partidas) . ' registros.');
+    }
+}
