76ed6cc1821722f17b6b18f9b3f9fad2bfba1aac
Ernesto Nicolás Carrea <equistango@gmail.com>
2016-01-14 11:58:08 -0300

    Mejoras en las vistas de listar, ver y pública ver

--- src/Yacare/NominaBundle/Controller/InmuebleController.php
+++ src/Yacare/NominaBundle/Controller/InmuebleController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Tapir\OsmBundle\Maps;
 
 /**
  * Controlador de inmuebles.
@@ -16,5 +17,45 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  */
 class InmuebleController extends \Tapir\AbmBundle\Controller\AbmController
 {
-    use \Tapir\AbmBundle\Controller\ConVer;
+use \Tapir\AbmBundle\Controller\ConVer {
+        \Tapir\AbmBundle\Controller\ConVer::verAction as parent_verAction;
+    }
+    
+    /**
+     * @Route("ver/")
+     * @Template()
+     */
+    public function verAction(Request $request)
+    {
+        $ResultadoVer = $this->parent_verAction($request);
+        $res = $ResultadoVer['res'];
+        $Inmueble = $res->Entidad;
+        if(!$Inmueble->getUbicacion()) {
+            $em = $this->getEm();
+            $Helper = new \Yacare\CatastroBundle\Helper\PartidaHelper($this->container, $em);
+            $Helper->ObtenerUbicacionPorDomicilio($Inmueble);
+            $em->flush();
+        }
+    
+        if($Inmueble->getUbicacion()) {
+            // Creo un mapa con la ubicación
+            $Mapa = new Maps\Map();
+            $Marcador = new Maps\Marker();
+            $Marcador->setPosition(new Maps\Point($Inmueble->getUbicacion()->getX(), $Inmueble->getUbicacion()->getY()));
+            $Marcador->setDescription($Inmueble->getDomicilioReal());
+            $Mapa->addMarker($Marcador);
+            $res->Mapa = $Mapa;
+        }
+        return $ResultadoVer;
+    }
+    
+    
+    /**
+     * @Route("publica/ver/")
+     * @Template("YacareNominaBundle:Inmueble:publica/ver.html.twig")
+     */
+    public function publica_verAction(Request $request)
+    {
+        return $this->parent_verAction($request);
+    }
 }
