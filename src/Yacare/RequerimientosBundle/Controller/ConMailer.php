3b33fffed5f23ce06606946499820189ed413154
Ernesto Nicolás Carrea <equistango@gmail.com>
2016-01-15 12:08:50 -0300

    Arreglos

--- src/Yacare/RequerimientosBundle/Controller/ConMailer.php
+++ src/Yacare/RequerimientosBundle/Controller/ConMailer.php
@@ -13,11 +13,8 @@ trait ConMailer
     /**
      * Agrega una novedad a un requerimiento y envía un e-mail al usuario si corresponde.
      * 
-     * @param \Yacare\RequerimientosBundle\Entity\Novedad $entidad      novedad nueva en el reqeurimiento.
-     * @param string                                      $vistaEmail        direción donde se aloja la plantilla del 
-     *                                                                       e-mail. 
-     * @param string                                      $numeroSeguimiento compuesto por la ID del requerimiento, y 
-     *                                                                       un token aleatorio.
+     * @param object $entidad      La novedad a notificar o el reqeurimiento.
+     * @param string $vistaEmail   El nombre de la plantilla para el mensaje de e-mail. 
      */
     protected function InformarNovedad($request, $entidad, 
         $vistaEmail = 'YacareRequerimientosBundle:Requerimiento/Mail:requerimiento_novedad.html.twig')
@@ -29,35 +26,33 @@ trait ConMailer
             $Requerimiento = $entidad;
             $Novedad = null;
         }
-        if($Requerimiento->getEmailNotificaciones()) {
+        
+        $Destinatarios = array();
+        if($Requerimiento->getEncargado() && $Requerimiento->getEncargado()->getEmail()) {
+            $Destinatarios[$Requerimiento->getEncargado()->getEmail()] = $Requerimiento->getEncargado()->NombreAmigable();
+        }
+        if($Requerimiento->getUsuario() && $Requerimiento->getUsuario()->getEmail()) {
+            $Destinatarios[$Requerimiento->getUsuario()->getEmail()] = $Requerimiento->getUsuario()->NombreAmigable();
+        } else {
+            if($Requerimiento->getUsuarioNombre()) {
+                $Destinatarios[$Requerimiento->getUsuarioNombre()] = $Requerimiento->getUsuarioEmail();
+            } else {
+                $Destinatarios['Usuario anónimo'] = $Requerimiento->getUsuarioEmail();
+            }
+        }
+        
+        if(count($Destinatarios) > 0) {
             $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoVerAction($this), $request);
             $res->Entidad = $Requerimiento;
             $res->Novedad = $Novedad;
-           
             $ContenidoMensaje = $this->renderView($vistaEmail, array('res' => $res)); 
-            
-            $Destinatarios = array();
-            if($Requerimiento->getEncargado() && $Requerimiento->getEncargado()->getEmail()) {
-                $Destinatarios[$Requerimiento->getEncargado()->getEmail()] = $Requerimiento->getEncargado()->NombreAmigable();
-            }
-            if($Requerimiento->getUsuario() && $Requerimiento->getUsuario()->getEmail()) {
-                $Destinatarios[$Requerimiento->getUsuario()->getEmail()] = $Requerimiento->getUsuario()->NombreAmigable();
-            } else {
-                if($Requerimiento->getUsuarioNombre()) {
-                    $Destinatarios[$Requerimiento->getUsuarioNombre()] = $Requerimiento->getUsuarioEmail();
-                } else {
-                    $Destinatarios['Usuario anónimo'] = $Requerimiento->getUsuarioEmail();
-                }
-            }
-            
-            if(count($Destinatarios) > 0) {
-                $Mensaje = \Swift_Message::newInstance()
-                    ->setSubject('Novedades de su solicitud')
-                    ->setFrom(array('reclamos@riogrande.gob.ar' => 'Municipio de Río Grande'))
-                    ->setTo($Destinatarios)
-                    ->setBody($ContenidoMensaje, 'text/html');
-                $this->get('mailer')->send($Mensaje);
-            }
+        
+            $Mensaje = \Swift_Message::newInstance()
+                ->setSubject('Novedades de su solicitud')
+                ->setFrom(array('reclamos@riogrande.gob.ar' => 'Municipio de Río Grande'))
+                ->setTo($Destinatarios)
+                ->setBody($ContenidoMensaje, 'text/html');
+            $this->get('mailer')->send($Mensaje);
         }
     }
 }
