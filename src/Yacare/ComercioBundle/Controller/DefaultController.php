d4cbe4520a449bac3aa15676eb7cfaf590b668eb
Ernesto Nicolás Carrea <equistango@gmail.com>
2016-01-19 11:42:36 -0300

    Separar resumen e inicio

--- src/Yacare/ComercioBundle/Controller/DefaultController.php
+++ src/Yacare/ComercioBundle/Controller/DefaultController.php
@@ -16,10 +16,10 @@ class DefaultController extends \Tapir\BaseBundle\Controller\DefaultController
     use \Tapir\AyudaBundle\Controller\ConAyuda;
     
     /**
-     * @Route("inicio/")
+     * @Route("resumen/")
      * @Template
      */
-    public function inicioAction(Request $request)
+    public function resumenAction(Request $request)
     {
         $res = $this->ConstruirResultado(new \Yacare\ComercioBundle\Helper\Resultados\ResultadoInicioAction($this),
             $request);
@@ -32,21 +32,35 @@ class DefaultController extends \Tapir\BaseBundle\Controller\DefaultController
 		LEFT JOIN c.Actividad1 a
 	WHERE c.Actividad1 IS NOT NULL
     GROUP BY c.Actividad1
-    ORDER BY cant DESC')->setMaxResults(10)->getResult();
+    ORDER BY cant DESC')->setMaxResults(15)->getResult();
         
-        $chart_personasporciudad = new Chartjs();
-    	$chart_personasporciudad->RenderTo = 'chart_sociosporciudad';
-    	$chart_personasporciudad->ChartType = 'Doughnut';
-    	$chart_personasporciudad
-    		->AddOption('scaleShowLabelBackdrop', true)
-			->AddOption('animateRotate', true)
-	    ;
-
-		foreach($res->Rankings['ComercioActividad'] as $Porcion) {
-			$chart_personasporciudad->AddPieValue($Porcion['Nombre'], $Porcion['cant']);
-		}
-		
-		$res->Charts['ComercioActividad'] = $chart_personasporciudad;
+        $chart_ComercioActividad = new Chartjs();
+        $chart_ComercioActividad->RenderTo = 'char_comercioactividad';
+        $chart_ComercioActividad->ChartType = 'Doughnut';
+        $chart_ComercioActividad
+        ->AddOption('scaleShowLabelBackdrop', true)
+        ->AddOption('animateRotate', true)
+        ;
+        
+        foreach($res->Rankings['ComercioActividad'] as $Porcion) {
+            $chart_ComercioActividad->AddPieValue($Porcion['Nombre'], $Porcion['cant']);
+        }
+        
+        $res->Charts['ComercioActividad'] = $chart_ComercioActividad;
+        
+        return array('res' => $res);
+    }
+    
+    /**
+     * @Route("inicio/")
+     * @Template
+     */
+    public function inicioAction(Request $request)
+    {
+        $res = $this->ConstruirResultado(new \Yacare\ComercioBundle\Helper\Resultados\ResultadoInicioAction($this),
+            $request);
+        
+        $this->ObtenerContadoresYRecientes($res);
         
         return array('res' => $res);
     }
