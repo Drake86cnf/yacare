5383431931faadd904092f734cb6a035aacafb8c
Ernesto Nicolás Carrea <equistango@gmail.com>
2016-01-29 11:40:32 -0300

    Requisitos por etapa

--- src/Yacare/TramitesBundle/Entity/Tramite.php
+++ src/Yacare/TramitesBundle/Entity/Tramite.php
@@ -336,7 +336,7 @@ abstract class Tramite implements ITramite
             if ($EstadoRequisito->EsNecesario()
                 && $EstadoRequisito->EsOpcional() == false
                 && $EstadoRequisito->EstaCumplido() == false
-                && ($EstadoRequisito->getAsociacionRequisito()->getEtapa() == null || $EstadoRequisito->getAsociacionRequisito()->getEtapa() == $etapa)) {
+                && ($etapa  == null || $EstadoRequisito->getAsociacionRequisito()->getEtapa() == $etapa)) {
                 $res ++;
             }
         }
