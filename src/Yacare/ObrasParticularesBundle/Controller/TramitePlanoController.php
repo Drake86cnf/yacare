ca9baffb744b067e994bb5ff01a629ec8241bf08
Ezequiel Riquelme <rezequiel.tdf@gmail.com>
2016-02-15 13:31:21 -0300

    Correción bugs de seguridad.

--- src/Yacare/ObrasParticularesBundle/Controller/TramitePlanoController.php
+++ src/Yacare/ObrasParticularesBundle/Controller/TramitePlanoController.php
@@ -12,39 +12,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
  * @author Ezequiel Riquelme <rezquiel.tdf@gmail.com>
  * 
  * @Route("tramiteplano/")
- * @Security("has_role('ROLE_IDDQD') or has_role('ROLE_OBRAS_PARTICULARES_ADMINISTRADOR')")
+ * @Security("has_role('ROLE_IDDQD') or has_role('ROLE_OBRAS_PARTICULARES_ADMINISTRADOR') or has_role('ROLE_OBRAS_PARTICULARES_MESA_DE_ENTRADA')")
  */
 class TramitePlanoController extends \Yacare\TramitesBundle\Controller\TramiteController
 {
-    //use \Yacare\AdministracionBundle\Controller\ConSeguimiento;
-
-    /**
-     * @Route("adjuntos/listar/")
-     * @Template("YacareObrasParticularesBundle:ActaObra:adjuntos_listar.html.twig")
-     */
-    public function adjuntoslistarAction(Request $request)
-    {
-        return parent::adjuntoslistarAction($request);
-    }
-
-    /**
-     * Editar un tramite de planos.
-     *
-     * @see \Tapir\AbmBundle\Controller\AbmController::editarAction() AbmController::editarAction()
-     *
-     * @Route("editar/")
-     * @Route("crear/")
-     * @Template()
-     */
-    public function editarAction(Request $request)
-    {
-        return parent::editarAction($request);
-    }
-
+    // use \Yacare\AdministracionBundle\Controller\ConSeguimiento;
+    
     /**
      * El inicio de obra de un tramite plano.
-     * 
+     *
      * @Route("iniciodeobra/")
+     * @Security("has_role('ROLE_IDDQD') or has_role('ROLE_OBRAS_PARTICULARES_ADMINISTRADOR')")
      * @Template()
      */
     public function iniciodeobraAction(Request $request)
@@ -64,15 +42,14 @@ class TramitePlanoController extends \Yacare\TramitesBundle\Controller\TramiteCo
         
         return array('res' => $res);
     }
-    
+
     /**
      * Caratula la previa de acuerod al Siaf.
-     * 
+     *
      * @Route("caratularprevia/")
-     * @Security("has_role('ROLE_IDDQD') or has_role('ROLE_OBRAS_PARTICULARES_ADMINISTRADOR') or has_role('ROLE_OBRAS_PARTICULARES_MESA_DE_ENTRADA')")
      * @Template()
      */
-    public function caratularpreviaAction(Request $request) 
+    public function caratularpreviaAction(Request $request)
     {
         $em = $this->getEm();
         $id = $this->ObtenerVariable($request, 'id');
@@ -91,20 +68,20 @@ class TramitePlanoController extends \Yacare\TramitesBundle\Controller\TramiteCo
             ->add('ExpedienteNumero', 'Yacare\AdministracionBundle\Form\Type\ExpedienteType', array(
                 'label' => 'Nº Expediente'))
             ->add('FechaAprobadaPrevia', 'Tapir\BaseBundle\Form\Type\FechaPasadoPresenteType', array(
-                'label' => 'Fecha aprobado',));
+                'label' => 'Fecha aprobado'));
         
         $FormEditar = $FormEditarBuilder->getForm();
         $FormEditar->handleRequest($request);
-
+        
         if ($FormEditar->isValid()) {
             $em->persist($Entidad);
             $em->flush();
-
+            
             return $this->guardarActionAfterSuccess($request, $Entidad);
         } else {
             $Errores = $FormEditar->getErrors(true, true);
         }
-
+        
         if ($Errores) {
             foreach ($Errores as $error) {
                 $this->get('session')->getFlashBag()->add('danger', $error->getMessage());
@@ -112,14 +89,57 @@ class TramitePlanoController extends \Yacare\TramitesBundle\Controller\TramiteCo
         } else {
             $Errores = null;
         }
-
-        $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoEditarGuardarAction($this),
+        
+        $res = $this->ConstruirResultado(new \Tapir\AbmBundle\Helper\Resultados\ResultadoEditarGuardarAction($this), 
             $request);
         $res->Entidad = $Entidad;
         $res->AccionGuardar = 'caratularprevia';
         $res->FormularioEditar = $FormEditar->createView();
         $res->Errores = $Errores;
-
+        
         return array('res' => $res);
+    }
+
+    /**
+     * @Route("listar/")
+     * @Template()
+     */
+    public function listarAction(Request $request)
+    {
+        return parent::listarAction($request);
+    }
+
+    /**
+     * @Route("ver/")
+     * @Template()
+     */
+    public function verAction(Request $request)
+    {
+        return parent::verAction($request);
+    }
+
+    /**
+     * Editar un tramite de planos.
+     *
+     * @see \Tapir\AbmBundle\Controller\AbmController::editarAction() AbmController::editarAction()
+     *
+     * @Route("editar/")
+     * @Route("crear/")
+     * @Security("has_role('ROLE_IDDQD') or has_role('ROLE_OBRAS_PARTICULARES_ADMINISTRADOR')")
+     * @Template()
+     */
+    public function editarAction(Request $request)
+    {
+        return parent::editarAction($request);
+    }
+
+    /**
+     * @Route("adjuntos/listar/")
+     * @Security("has_role('ROLE_IDDQD') or has_role('ROLE_OBRAS_PARTICULARES_ADMINISTRADOR')")
+     * @Template("YacareObrasParticularesBundle:ActaObra:adjuntos_listar.html.twig")
+     */
+    public function adjuntoslistarAction(Request $request)
+    {
+        return parent::adjuntoslistarAction($request);
     }
 }
